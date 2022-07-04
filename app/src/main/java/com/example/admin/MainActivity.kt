545634a65d228f249ec868683941a5a959a47c35
rihyun1126 <rihyun1126@gmail.com>
2022-07-04 15:25:42 +0900

    안전모 미착용시 알림

--- app/src/main/java/com/example/admin/MainActivity.kt
+++ app/src/main/java/com/example/admin/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity.class"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getFCMToken()
+    }
+
+    private fun getFCMToken(): String?{
+        var token: String? = null
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            token = task.result
+
+            // Log and toast
+            Log.d(TAG, "FCM Token is ${token}")
+        })
+
+        return token
     }
 }
