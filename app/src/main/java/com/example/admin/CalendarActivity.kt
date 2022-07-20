bf2a3c727c0ec030e2b38ee83e5fc6a60b2e8895
rihyun1126 <rihyun1126@gmail.com>
2022-07-20 18:54:57 +0900

    calendar 데이터 넣기

--- app/src/main/java/com/example/admin/CalendarActivity.kt
+++ app/src/main/java/com/example/admin/CalendarActivity.kt
@@ -18,12 +18,14 @@ class CalendarActivity : AppCompatActivity() {
         binding = ActivityCalendarBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calendarOkBtn.setOnClickListener {
-            val work = binding.calendarEditBtn.text.toString()// 사용자가 입력한 작업
+            val startdate = binding.calendarStartdate.text.toString()// 사용자가 입력한 작업
+            val enddate = binding.calendarEnddate.toString()
+            val contents = binding.calendarContents.text.toString()
 
             // Volley를 이용한 http 통신
             val calendaruploadRequest = object : StringRequest(
                 Request.Method.POST,
-                "http://ip주소/calendarupload.php",
+                "http://172.20.10.4/calendar_upload.php",
                 Response.Listener<String>{ response ->
                     if(response.toInt() == 1){ // 성공
                         Toast.makeText(this, "성공.", Toast.LENGTH_LONG).show()
@@ -35,7 +37,9 @@ class CalendarActivity : AppCompatActivity() {
                 }){
                 override fun getParams(): MutableMap<String, String>? { // API로 전달할 데이터
                     val params : MutableMap<String, String> = HashMap()
-                    params["work"] = work
+                    params["startdate"] = startdate
+                    params["enddate"] = enddate
+                    params["contents"] = contents
                     return params
                 }
             }
